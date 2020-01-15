b834bbaa9637a567aff10baa8358f092de5a8ad7
Evgeny <fetel@yandex.ru>
2020-01-15 19:02:56 +0300

    created custom behaviour to consume scroll

--- app/src/main/java/com/simple/scroll/CustomBehaviour.kt
+++ app/src/main/java/com/simple/scroll/CustomBehaviour.kt
@@ -1,11 +1,13 @@
 package com.simple.scroll
 
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
 import com.google.android.material.appbar.AppBarLayout
-
 import kotlin.math.abs
 
 
@@ -14,7 +16,27 @@ class CustomBehaviour @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ) : AppBarLayout.ScrollingViewBehavior(context, attrs) {
 
-    val scroll = context.resources.getDimensionPixelSize(R.dimen.cell_height)
+    companion object {
+        private const val ANIMATION_DURATION: Long = 150
+        private const val VALUE_FROM_INDEX = 0
+        private const val VALUE_TO_INDEX = 1
+    }
+
+    var allowFreeSpaceAtTop: Boolean = false
+        set(value) {
+            if (field != value) {
+                field = value
+                updateShortcutsState(value)
+            }
+        }
+
+    /**total scroll distance allowed for consume*/
+    private val scrollDistance = context.resources.getDimensionPixelSize(R.dimen.cell_height)
+    /**value between 0 and [scrollDistance]*/
+    private var consumedScroll = 0
+
+    private var childViewPadding = Rect()
+    private var dependencyView: View? = null
 
     override fun layoutDependsOn(
         parent: CoordinatorLayout,
@@ -23,25 +45,138 @@ class CustomBehaviour @JvmOverloads constructor(
     ): Boolean {
         return super.layoutDependsOn(parent, child, dependency).also { depends ->
             if (depends) {
-                (dependency as AppBarLayout).addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appbarLayout, verticalOffset ->
-                    (child as CollapsedEventProvider).isCollapsed =
-                        abs(verticalOffset) - appbarLayout.totalScrollRange == 0
-                })
+                dependencyView = child
+                childViewPadding.left = child.paddingLeft
+                childViewPadding.right = child.paddingRight
+                childViewPadding.bottom = child.paddingBottom
             }
         }
     }
 
-//    override fun onDependentViewChanged(
-//        parent: CoordinatorLayout,
-//        child: View,
-//        dependency: View
-//    ): Boolean {
-//        Log.d("fuck", "onDependentViewChanged")
-//        if (collapsed) {
-//            (child as NestedScrollView).smoothScrollBy(0, scroll)
-//        }
-//
-//        return super.onDependentViewChanged(parent, child, dependency)
-//    }
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: View,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return (axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(
+                    coordinatorLayout,
+                    child,
+                    directTargetChild,
+                    target,
+                    axes,
+                    type
+                )) && allowFreeSpaceAtTop
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: View,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        val canConsume = if (dy > 0) {
+            consumedScroll > 0
+        } else {
+            scrollDistance - consumedScroll > 0
+        }
+
+        if (!canConsume) {
+            return
+        }
+
+        val consumedDx = 0
+        val consumedDy: Int = when {
+            dy > 0 -> {
+                //scroll down
+                val maxConsumeAmount = consumedScroll
+
+                if (maxConsumeAmount == 0) {
+                    //free space fully expanded, no need to consume scroll
+                    return
+                }
+
+                if (dy > maxConsumeAmount) {
+                    maxConsumeAmount
+                } else {
+                    dy
+                }.also {
+                    consumedScroll -= it
+                }
+            }
+            dy < 0 -> {
+                //scroll up
+                val maxConsumeAmount = scrollDistance - consumedScroll
+
+                if (maxConsumeAmount == 0) {
+                    //free space fully collapsed, no need to consume scroll
+                    return
+                }
+
+                if (abs(dy) > maxConsumeAmount) {
+                    -maxConsumeAmount
+                } else {
+                    dy
+                }.also {
+                    consumedScroll += -it
+                }
+            }
+
+            else -> {
+                super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+                return
+            }
+        }
+
+        consumed[0] = consumedDx
+        consumed[1] = consumedDy
+
+        dependencyView?.updatePadding(consumedScroll)
+
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+    }
+
+    private fun updateShortcutsState(hasSpace: Boolean) {
+        dependencyView?.run {
+            val pair = if (hasSpace) {
+                consumedScroll = scrollDistance
+                arrayOf(0, scrollDistance)
+            } else {
+                consumedScroll = 0
+                arrayOf(scrollDistance, 0)
+            }
+
+            val valueTo = pair[VALUE_TO_INDEX]
+            val valueFrom = if (pair[VALUE_FROM_INDEX] == consumedScroll) {
+                pair[VALUE_FROM_INDEX]
+            } else {
+                consumedScroll
+            }
+
+            ValueAnimator.ofInt(valueFrom, valueTo).apply {
+                removeAllUpdateListeners()
+                addUpdateListener {
+                    val updateValue = it.animatedValue as Int
+                    updatePadding(updateValue)
+                }
+                duration = ANIMATION_DURATION
+            }.start()
+        }
+    }
+
+    private fun View.updatePadding(topPadding: Int) {
+        setPadding(
+            childViewPadding.left,
+            topPadding,
+            childViewPadding.right,
+            childViewPadding.bottom
+        )
+    }
 
 }
--- app/src/main/java/com/simple/scroll/MainActivity.kt
+++ app/src/main/java/com/simple/scroll/MainActivity.kt
@@ -2,27 +2,51 @@ package com.simple.scroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
+import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.contains
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val cellCount = 15
+    var customBehaviour: CustomBehaviour? = null
+
+    private var moreSpace = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupRecyclerView()
+
+        customBehaviour =
+            ((rvContainer.layoutParams as CoordinatorLayout.LayoutParams).behavior as CustomBehaviour)
+
+        fab.setOnClickListener {
+            if (moreSpace) {
+                toast("remove some space ")
+                customBehaviour?.allowFreeSpaceAtTop = false
+                moreSpace = false
+            } else {
+                toast("add some space")
+                customBehaviour?.allowFreeSpaceAtTop = true
+                moreSpace = true
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        customBehaviour?.allowFreeSpaceAtTop = moreSpace
+    }
+
+    private fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
     private fun setupRecyclerView() {
         recyclerView.run {
-            layoutManager = LinearLayoutManager(this@MainActivity)
+            layoutManager = LinearLayoutManager(context)
             adapter = RecyclerAdapter(cellCount)
             setHasFixedSize(true)
         }
